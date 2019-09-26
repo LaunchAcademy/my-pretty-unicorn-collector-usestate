f04fb914479bed88593a8308b28fcd95e0791d5a
Cassandra Newell <cassandra.newell@protonmail.com>
2019-09-26 13:16:25 -0400

    prep for release to cohort

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './main.scss';
+import App from './components/App';
+import '../public/main.scss';
 
 
 ReactDOM.render(
